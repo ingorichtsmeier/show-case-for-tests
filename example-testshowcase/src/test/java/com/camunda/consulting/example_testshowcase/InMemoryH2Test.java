724cccb0099431ecd997d6f0838be5fe3b5c8039
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2019-05-09 17:27:07 +0200

    initial version

---
+++ example-testshowcase/src/test/java/com/camunda/consulting/example_testshowcase/InMemoryH2Test.java
@@ -0,0 +1,70 @@
+package com.camunda.consulting.example_testshowcase;
+
+import org.apache.ibatis.logging.LogFactory;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.camunda.bpm.engine.test.Deployment;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.junit.Assert.*;
+
+/**
+ * Test case starting an in-memory database-backed Process Engine.
+ */
+public class InMemoryH2Test {
+
+  @ClassRule
+  @Rule
+  public static ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
+
+  private static final String PROCESS_DEFINITION_KEY = "example-testshowcase";
+
+  static {
+    LogFactory.useSlf4jLogging(); // MyBatis
+  }
+
+  @Before
+  public void setup() {
+    init(rule.getProcessEngine());
+  }
+
+  /**
+   * Just tests if the process definition is deployable.
+   */
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testParsingAndDeployment() {
+    // nothing is done here, as we just want to check for exceptions during deployment
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testHappyPath() {
+	  ProcessInstance processInstance = runtimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+	  
+	  // Now: Drive the process by API and assert correct behavior by camunda-bpm-assert
+	  assertThat(processInstance).isWaitingAt("UserTask1Task").task().hasName("user task 1");
+	  complete(task());
+	  assertThat(processInstance).isWaitingAt("UserTask2Task").task().hasName("user task 2");
+	  complete(task(), withVariables("prc_ok", true));
+	  assertThat(processInstance).isEnded();
+  }
+  
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testPrcNotOK() {
+    ProcessInstance processInstance = runtimeService()
+        .createProcessInstanceByKey(PROCESS_DEFINITION_KEY)
+        .startAfterActivity("UserTask2Task")
+        .setVariables(withVariables("prc_ok", false))
+        .execute();
+    
+    assertThat(processInstance).isWaitingAt("UserTask1Task");
+  }
+
+}
